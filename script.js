4b029abe43ee95d2b8d1df06357b5965918cb632
ezekiel <florian.vidal@lapiscine.pro>
2025-08-16 11:45:37 +0200

    <commentaire>

--- script.js
+++ script.js
@@ -38,9 +38,42 @@ const projets = [
     lien: "#",
   },
   {
-    titre: "Les Zaniméo",
+    titre: "Application pour s'entraîner : Agence immobilière",
     description: "Une application ludo-éducative pour enfants",
+    lien: "www.agence-immo-site.com",
+  },
+  {
+    titre: "Jeu intéractif : Les Zaniméo",
+    description: "Une application ludo-éducative pour enfants, créé ",
     lien: "https://leszanimeo.com/",
+  },
+  {
+    titre: "Site pour collectionneurs : DBZ Museum Collection",
+    description:
+      "Projet qui a pour but de rendre hommage à une licence culte des années 90, j'ai nommé le manga Dragon Ball. Ce site a été pensé et élaboré selon une l'idée d'un musée qui référence les objets que l'on trouvait sur cette licence en France.",
+    date: "Janvier 2019",
+    lien: "https://dbzmuseumcollection.wixsite.com/website-1",
+  },
+  {
+    titre: "Librairie : Le Cercle Des Poètes",
+    description: "Une application ludo-éducative pour enfants, créé ",
+    lien: "www.le-cercle-des-poetes.com",
+  },
+  {
+    titre: "Commande client : Cabinet Kiné-Massage-Ostéo",
+    description: "Une application ludo-éducative pour enfants, créé ",
+    lien: "www.cabinetcazaux.fr",
+  },
+  {
+    titre: "Commande client : Réalisateur, producteur, médias",
+    description: "Une application ludo-éducative pour enfants, créé ",
+    lien: "www.mathieupichoff.com",
+  },
+  {
+    titre:
+      "Création site pour mon ancienne société : Boutique spécialisée dans le CBD",
+    description: "Une application ludo-éducative pour enfants, créé ",
+    lien: "www.evasioncbd.com",
   },
 ];
 
@@ -58,6 +91,9 @@ projets.forEach((projet) => {
   const desc = document.createElement("p");
   desc.textContent = projet.description;
 
+  const date = document.createElement("p");
+  date.textContent = projet.date;
+
   const link = document.createElement("a");
   link.href = projet.lien;
   link.textContent = "Voir le projet";
@@ -66,6 +102,7 @@ projets.forEach((projet) => {
   // On assemble tous les éléments dans la carte
   card.appendChild(title);
   card.appendChild(desc);
+  card.appendChild(date);
   card.appendChild(link);
 
   // On ajoute la carte au conteneur
