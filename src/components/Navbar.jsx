5370af27e9e9a570b8d1c4e96a2b78b00741888d
ezekiel <florian.vidal@lapiscine.pro>
2025-09-29 16:10:25 +0200

    responsive + design + cv page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,42 @@
 // Import du composant Link de React Router qui permet de créer des liens internes sans recharger la page
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false); // Initialisation d'un state pour gérer le menu burger
+
   return (
     // Balise de navigation principale
     <nav className="navbar">
       {/* Logo ou nom affiché dans la navbar */}
-      <Link to="/">
+      <NavLink to="/">
         <div className="logo">
           <img src="/logo.webp" />
           <h1>Florian Vidal - Développeur Web Fullstack</h1>
         </div>
-      </Link>
+      </NavLink>
 
-      {/* Liste des liens de navigation */}
-      <ul>
-        {/* Chaque <li> contient un lien interne avec Link */}
+      {/* Bouton menu burger (visible seulement en mobile via le CSS) */}
+      <button className="burger" onClick={() => setIsOpen(!isOpen)}>
+        ☰
+      </button>
+
+      {/* Menu navigation */}
+      <ul className={isOpen ? "nav-links open" : "nav-links"}>
         <li>
-          <Link to="/projects">Projets</Link>
+          <NavLink to="/projects" className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`} onClick={() => setIsOpen(false)}>
+            Projets
+          </NavLink>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <NavLink to="/CV" className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`} onClick={() => setIsOpen(false)}>
+            CV
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact" className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`} onClick={() => setIsOpen(false)}>
+            Contact
+          </NavLink>
         </li>
       </ul>
     </nav>
